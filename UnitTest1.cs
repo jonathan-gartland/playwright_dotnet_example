2292d6df1c89a1b5e5674928b93e18d324f40cfd
jonathan-gartland <jonathan-gartland@gmail.com>
2023-12-05 16:26:12 -0500

    just getting familiar with the .NET Playwright ecosystem. will replicate tests from react app here

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -4,18 +4,37 @@ using Microsoft.Playwright;
 using Microsoft.Playwright.MSTest;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using System;
+using System.Threading;
+using Microsoft.Playwright.Core;
+using Microsoft.Playwright.TestAdapter;
+
+
 namespace PlaywrightTests;
 
 [TestClass]
 public class UnitTest1 : PageTest
 {
-    [TestMethod]
-    public async Task MpnAppTest()
-    {
+    [TestInitialize]
+    // public async Task Setup()
+    // {
+    //     await Page.GotoAsync("https://jonathan-gartland.github.io/practice-pages");
+    // }
 
+    [TestMethod]
+    public async Task MpnAppTitleTest()
+    {
         await Page.GotoAsync("https://jonathan-gartland.github.io/practice-pages");
         var pageTitle = Page.Locator("text=MPN Lookup For QuantiTray\u00ae Product Suite");
         await Expect(pageTitle).ToBeVisibleAsync();
+
+    }
+    
+    [TestMethod]
+    public async Task MpnAppAboutTest()
+    {
+        await Page.GotoAsync("https://jonathan-gartland.github.io/practice-pages");
+        await Expect(Page).ToHaveURLAsync(new Regex(".*practice-pages"));
 
     }
 }
