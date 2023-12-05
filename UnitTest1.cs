c4ffbd031829ca8375fa66b99cc3fd0ca03110af
jonathan-gartland <jonathan-gartland@gmail.com>
2023-12-05 14:55:16 -0500

    first commit csharp dotnet playwright example

---
+++ UnitTest1.cs
@@ -0,0 +1,19 @@
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.MSTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PlaywrightTests;
+
+[TestClass]
+public class UnitTest1 : PageTest
+{
+    [TestMethod]
+    public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage()
+    {
+
+        await Page.GotoAsync("https://jonathan-gartland.github.io/practice-pages");
+
+    }
+}
